990b0416211a485bbd37d271ba7ab20fd70b7fd3
EdwinUgarte <Edwin112098>
2023-02-20 21:17:21 -0600

    mejoras visuales

--- src/app/personajes/pages/home/home.component.ts
+++ src/app/personajes/pages/home/home.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PersonajesService } from '../../services/personajes.service';
+import { Personaje } from '../../interfaces/personaje.interface';
+
+import {Router} from '@angular/router'
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+
+  rick!: Personaje;
+  morty!: Personaje;
+
+  constructor(private personajeService: PersonajesService, private router: Router){}
+
+  ngOnInit(){
+     this.getRick();
+     this.getMorty();
+  }
+
+
+  getRick(){
+    this.personajeService.buscarPersonaje("1").subscribe(personaje => {
+      this.rick = personaje;
+    })
+  }
+
+
+  getMorty(){
+    this.personajeService.buscarPersonaje("2").subscribe(personaje => {
+      this.morty = personaje;
+    })
+  }
+
+
+  navegar(id: any){
+    this.router.navigate([`personaje/${id}`])
+    
+  }
 
 }
--- src/app/personajes/personajes.module.ts
+++ src/app/personajes/personajes.module.ts
@@ -4,6 +4,7 @@ import { HomeComponent } from './pages/home/home.component';
 import { ListadoComponent } from './pages/listado/listado.component';
 import { PersonajeComponent } from './pages/personaje/personaje.component';
 import { CardComponent } from './components/card/card.component';
+import {RouterModule} from '@angular/router';
 
 
 
@@ -15,7 +16,8 @@ import { CardComponent } from './components/card/card.component';
     CardComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ]
 })
 export class PersonajesModule { }
